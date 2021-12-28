29de4b3690ca9e05e813f8f0ff52afe46e2bf137
Toni Hermoso Pulido <toniher@softcatala.org>
2021-12-28 21:25:26 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,11 +5,13 @@ export DB=/nfs/db/kraken2/hittheleria
 # Singularity image
 export SING="singularity exec -e /software/bi/singularity/kraken2/kraken2-202112.sif"
 
+export ADDFASTA="/nfs/db/kraken2/tmp/ncbi-genomes-2021-12-16/GCF_002263795.1_ARS-UCD1.2_genomic.fna"
+
 # Taxonomy
-$SING kraken2-build --download-taxonomy --db $DB
+$SING kraken2-build --download-taxonomy $ADDFASTA --db $DB
 
 # Additional files
-$SING kraken2-build --add-to-library /nfs/db/kraken2/tmp/ncbi-genomes-2021-12-16/GCF_002263795.1_ARS-UCD1.2_genomic.fna -db $DB
+$SING kraken2-build --add-to-library -db $DB
 
 # Defined groups
 orgs=( viral bacteria archaea fungi protozoa human UniVec_Core )
